3dae588e7496e1f1e16166c3c28538eca86b15f1
Ardec <Kamil.Kaczmarczyk.box@gmail.com>
2023-09-09 09:36:53 +0200

    tailwind postcss test

--- app.vue
+++ app.vue
@@ -1,9 +1,16 @@
 <template>
   <div>
+    <h1 class="bg-green-800">APP VUE</h1>
+    <h1 class="red">APP VUE</h1>
     <NuxtLayout>
-          <NuxtPage />
-           <!-- <NuxtWelcome /> -->
+      <NuxtPage />
+      <!-- <NuxtWelcome /> -->
     </NuxtLayout>
-
   </div>
 </template>
+<style lang="postcss" scoped>
+.red {
+  @apply bg-red-100;
+  @apply p-4;
+}
+</style>
