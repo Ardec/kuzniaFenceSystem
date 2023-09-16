af665af4781b712902dd333c9f2601d10ead59a6
Ardec <Kamil.Kaczmarczyk.box@gmail.com>
2023-09-16 23:47:52 +0200

    16_09

--- components/Visualisation.vue
+++ components/Visualisation.vue
@@ -11,14 +11,19 @@
             <input v-model="fenceHeight" type="number" id="wysokość_przesla" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-4 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Wysokość" :min="fenceMinHeight" :max="fenceMaxHeight" @blur="handleInput($event, 'height')" required>
         </div>
     </div>
-    <div class="draw-container m-4 bg-blue-500">
-      <div class="draw">
-
+    <div class="draw-container mx-4 flex">
+      <div :style="'height:'+fenceHeight/10+'px'" class="vertical-text m-2">{{fenceHeight}} mm</div>
+      <div :style="'width:'+fenceWidth/10+'px;height:'+fenceHeight/10+'px;'" class="draw mt-8 mr-8">
+      <div class="channels"></div>
       </div>
-    width: {{fenceWidth}}
-    height: {{fenceHeight}}
     </div>
-    
+      <div :style="'width:'+fenceWidth/10+'px;'" class="horizontal-text mx-12">{{fenceWidth}} mm</div>
+      <div class="bottom-navigation m-4 flex">
+        <button class="add-btn text-white px-4 py-2 m-8"><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path fill-rule="evenodd" clip-rule="evenodd" d="M19 13H13V19H11V13H5V11H11V5H13V11H19V13Z" fill="white"/>
+</svg> <span>Dodaj panel</span></button>
+      </div>
+
   </div>
 </template>
 
@@ -58,8 +63,41 @@ const handleInput = (event: Event, type: String) => {
 </script>
 
 <style scoped>
-.visualisation-container{
-  /* padding:11px 16px; */
+.vertical-text{
+  min-height:160px;
+  width: 20px;
+  writing-mode: vertical-rl; /* tekst będzie pisać od prawej do lewej, ale nadal będzie czytany od góry do dołu */
+  transform: rotate(180deg); /* obróć div, aby tekst był czytelny od góry do dołu */
+  text-align: center; /* centruje tekst w poziomie */
+}
+.draw{
+  border-bottom:dotted #1369A8 2px;
+  border-left:dotted #1369A8 2px;
+  display:flex;
+}
+.draw-container{
+  align-items: center;
+}
+.horizontal-text{
+  text-align: center;
+}
+.channels{
+  margin:0px 0px 0px 1px;
+  flex: 1;
+  border-left:solid #3F3E3F 4px;
+  border-right:solid #3F3E3F 4px;
+}
+.bottom-navigation{
+  background: #F0F0F0;
+box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+justify-content: center;
+}
+.add-btn{
+display: inline-flex;
+align-items: flex-start;
+border-radius: 4px;
+background: #1369A8;
+text-transform: uppercase;
 }
 
 </style>
