488021d269101cca87a7682373f386d80fc6ca78
Ardec <Kamil.Kaczmarczyk.box@gmail.com>
2023-09-22 15:44:15 +0200

    modalfix

--- components/FenceModal.vue
+++ components/FenceModal.vue
@@ -140,6 +140,8 @@ const handleInput = (event: Event, type: String) => {
 .modal-overlay {
   position: fixed;
   z-index:1000;
+  overflow-y: auto;
+  touch-action: none;
   top: 0;
   left: 0;
   width: 100%;
