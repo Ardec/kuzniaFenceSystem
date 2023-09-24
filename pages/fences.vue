a98f561666f10fac3e9704e9b57ec98cde439aa1
Ardec <Kamil.Kaczmarczyk.box@gmail.com>
2023-09-24 16:19:06 +0200

    eidt_mdl

--- pages/fences.vue
+++ pages/fences.vue
@@ -6,18 +6,10 @@
     :bodyText="'Przęsła Kuźnia można montować wewnątrz ram bramy przesuwnej, dwuskrzydłowej, furtki, balkonu, ramy na wymiar, bądź do samych słupów. Podaj najpierw szerokość oraz wysokość przęsła, które chcesz uzyskać.'"
     />
 
-      <TextBlock
-    :titleText="'kreator przęseł'"
-    :headerText="'Mini wizualizacja'"
-    :bodyText="'Tak może wyglądać Twoje przęsło w widoku 2D od frontu. Elementy z prawej i lewej to ceowniki trzymające konstrukcję. Aby rozpocząć wciśnij przycisk dodaj element'"
-    />
+   
 
     <Visualisation />
-      <TextBlock
-    :titleText="'kreator przęseł'"
-    :headerText="'Przybliżenie x6'"
-    :bodyText="'Poniżej przęsła zostały rozpisane w przybliżonym widoku aby można było dokładnie określić wymiary oraz przerwy. Domyślna przerwa to 20mm'"
-    />
+
         <TextBlock
     :titleText="'kreator przęseł'"
     :headerText="'Przerwy'"
